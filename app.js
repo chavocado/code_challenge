f42bbd299cac32beb8f5cfdf33ddbbc0ec19f91d
chavocado <d.chavez562@yahoo.com>
2017-05-30 14:28:38 -0500

    zip code api call to console, click/change event listeners added

--- app.js
+++ app.js
@@ -1,29 +1,53 @@
 $(document).ready(function() {
+// console.log('dom ready');
+//global variables
+var zip = null;
+
+//monitoring click events on zip input
+$('#submit').on('click', {zipCode:zip} , weather);
+//monitoring changes to zip input
+$('#zip').on('change', function(){
+    zip = parseInt($("#zip").val(), 10);
+    // console.log(zip);
+});
 
 });
+// console.log('first');
+
 
 //get weather function
 function weather() {
+    //api key
+    var key = '&APPID=bafc05054926bcbddaf0ed250d7ef6d5';
+    console.log(zip);
     //construct our URL
     var baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
-    var query = zipCode + ',us';
-    var request = baseURL + query;
+    var query = zip.value + ',us';
+    var request = baseURL + query + key;
     console.log(request);
-
+    //weather image request url to be added after success, .then
     $.ajax({
         type: "GET",
         url: encodeURI(request),
-        dataType: "jsonp",
-        crossDomain: true,
-        jsonp: 'json_callback',
         success: function(data) {
             console.log(data);
+            //upon success calculate farenheit and append weather values to DOM goes here.
         }
     });
 }
+
 
 
 //check callback
 function searchCallback(data) {
     console.log(data);
 }
+
+
+
+
+//Form validation for appointment
+
+//conditional statement that runs through form input values in form object
+//upon true clear form DOM and append Thank you template in it's place.
+//else alert user fields are required to schedule an appointment.
